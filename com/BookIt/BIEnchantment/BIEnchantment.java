24e832209f03a96e7ebeab3fc069ed97f78cf380
8bookit8 <8bookit8@gmail.com>
2021-07-26 23:45:48 +0900

    Test Commit

---
+++ com/BookIt/BIEnchantment/BIEnchantment.java
@@ -0,0 +1,12 @@
+package com.BookIt.BIEnchantment;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BIEnchantment extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        //TODO: register Enchantments
+    }
+
+
+}
